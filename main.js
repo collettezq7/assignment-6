7f658e6655b945e2cb0e048e05b13506f2c23fe2
Seth Howard <smiles.seth@gmail.com>
2015-02-25 21:59:07 -0700

    try this

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
 $('input').on('keyup', function (evt) {
-    $("#search").autocomplete({
-        source: function (request, response) {
-
-
-            $.ajax({
-                url: "http://api.bing.net/qson.aspx?Query=" + encodeURI(request.term) + "&JsonType=callback&JsonCallback=?",
-                dataType: "jsonp",
-   
-
-                success: function (data) {
-                    var returned = [];
-                    $.each(data.SearchSuggestion.Section, function (i, val) {
-                        returned.push(val.Text);
-                    });
-                    response(returned);
-                }
+    $.ajax({
+        url: "http://api.bing.net/qson.aspx?Query=" + encodeURI(request.term) + "&JsonType=callback&JsonCallback=?",
+        dataType: "jsonp",
+        success: function (data) {
+            $.each(data.SearchSuggestion.Section, function (i, val) {
+                $('.results').append('<div>' + val.Text + '</div>');
             });
         }
     });
