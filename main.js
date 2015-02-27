9772fee1f1d05f9b6687464489f6adb9b7e17898
collettezq7 <colleensmith@csmith.local>
2015-02-26 23:18:33 -0700

    Finally did it. Thanks again for the help
    Final upload. Good riddance Bing!

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-$('input').on('keyup', function (evt) {
-    $("#search").autocomplete({
-        source: function (request, response) {
 
 
-            $.ajax({
-                url: "http://api.bing.net/qson.aspx?Query=" + encodeURI(request.term) + "&JsonType=callback&JsonCallback=?",
-                dataType: "jsonp",
-   
+var url = "http://api.bing.net/qson.aspx?Query=";
+var endUrl = "&JsonType=callback&JsonCallback=?";
+//url components
 
-                success: function (data) {
-                    var returned = [];
-                    $.each(data.SearchSuggestion.Section, function (i, val) {
-                        returned.push(val.Text);
-                    });
-                    response(returned);
-                }
-            });
-        }
+$('input').on('keyup', function (evt){
+//event listener
+    $.ajax({
+        url : url + $(this).val() + endUrl,
+        dataType: "jsonp",
+        type: "GET"
+    }).done(function(response){
+    	var results = response.SearchSuggestion.Section;
+        console.log(response.SearchSuggestion.Section);
+        $(".results").empty();
+// ajax call to return results and empty when delete key is hit
+
+
+        $.each(results, function(i, value){
+        	console.log(value.Text);
+//jquery for loop 
+       
+        var html = $("<a>" + value.Text + "</a>");
+        $(".results").append(html);
+        	$("a").attr("href", encodeURI("http://www.bing.com/search + value.Text"));
+//turn to html and append DOM
+      });  
     });
 });
+
